d520aa648b7bec250d05f601fdc1fc16af5e91c8
Lucas Bazilio <45127111+lucasebazilio@users.noreply.github.com>
2023-04-24 11:16:34 +0200

    Update insertionProbes.py

--- NoResize/Probes and Collisions/Experiments/insertionProbes.py
+++ NoResize/Probes and Collisions/Experiments/insertionProbes.py
@@ -4,13 +4,13 @@ import timeit
 s = set()
 
 # Run the test num_run times and calculate the average time
-num_runs = 50
+num_runs = 3
 total_time = 0
 
 data = set(random.sample(range(100000),65529))
 
 for i in range(num_runs):
+    s = set()
     data = set(random.sample(range(100000), 65529))
     for x in data: s.add(x)
     s.add(100101)
-    s.clear()
