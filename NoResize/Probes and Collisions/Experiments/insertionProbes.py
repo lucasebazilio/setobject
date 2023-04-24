92828f0c13182a8331a9c85bbffba154e7152484
Lucas Bazilio <45127111+lucasebazilio@users.noreply.github.com>
2023-04-24 11:17:20 +0200

    Update insertionProbes.py

--- NoResize/Probes and Collisions/Experiments/insertionProbes.py
+++ NoResize/Probes and Collisions/Experiments/insertionProbes.py
@@ -14,3 +14,7 @@ for i in range(num_runs):
     data = set(random.sample(range(100000), 65529))
     for x in data: s.add(x)
     s.add(100101)
+
+    
+# Results
+# B = 9 -> Linear Probes: 958004  Random Probes: 80549
