1d6c22752799889d27921c7fbb065eefe2ed1022
Lucas Bazilio <45127111+lucasebazilio@users.noreply.github.com>
2023-04-29 02:25:49 +0200

    Update insertionProbes.py

--- NoResize/Probes and Collisions/Experiments/insertionProbes.py
+++ NoResize/Probes and Collisions/Experiments/insertionProbes.py
@@ -4,17 +4,17 @@ import timeit
 s = set()
 
 # Run the test num_run times and calculate the average time
-num_runs = 3
+num_runs = 400
 total_time = 0
 
-data = set(random.sample(range(100000),65529))
 
 for i in range(num_runs):
     s = set()
-    data = set(random.sample(range(100000), 65529))
-    for x in data: s.add(x)
-    s.add(100101)
-
+    data = set(random.sample(range(100000), 65535))   
+    for x in data: s.add(x) # amount = K-1
+    s.add(100101)           # amount = K
     
-# Results
-# B = 9 -> Linear Probes: 958004  Random Probes: 80549
+ 
+# Once size K is reached , total number of linear probes and random probes are displayed
+    
+ 
