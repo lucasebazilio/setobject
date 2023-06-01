ef9a23e657ecfd2f194ed6fd46adde88ae1f10ad
Lucas Bazilio <45127111+lucasebazilio@users.noreply.github.com>
2023-06-01 09:48:40 +0200

    Compute Probes Unsuccessful Search

--- NoResize/Probes and Collisions/Experiments/unsuccessfulsearchProbes.py
+++ NoResize/Probes and Collisions/Experiments/unsuccessfulsearchProbes.py
@@ -2,15 +2,18 @@ import random
 
 num_runs = 400
 
-
 for i in range(num_runs):
-    # Create a set with random even elements (S elements inserted)
-    data = set(random.sample(range(0, 200000, 2),63000))
+    # Create a set with random even elements
+    data = set(random.sample(range(0, 200000, 2),1999))
 
-    # Perform S lookups
     times = 0
-    while (times < 63000):
+    while (times < 2000):   # Perform S searches
         odd_number = random.randint(1, 200000) * 2 + 1
         c = odd_number in data
         times = times + 1
-    data.add(200001)    # S + 1 elements inserted, print probes
+    data.add(200001)
+
+
+
+
+
